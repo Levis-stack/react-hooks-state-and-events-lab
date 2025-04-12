ed2ea5fa6e96a4e5193527b240a3c243768b2917
Levis <Levis.mwangi@student.moringaschool.com>
2025-04-12 15:23:29 +0300

    passed test

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false);
+  const handleClick = () => {
+    setIsAdded((prev) => !prev);
+  };
+  const itemClass = isAdded ? "in-cart" : "";
+  const buttonClass = isAdded ? "remove" : "add";
+  const buttonText = isAdded ? "Remove from Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
